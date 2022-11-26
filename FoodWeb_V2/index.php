89192a24909b3707b09fb7222a4334ec6af9c89d
ideasender <tomy.ideasender@gmail.com>
2022-11-26 13:54:00 +0700

    [PROGRESS] : Update php permission

--- FoodWeb_V2/index.php
+++ FoodWeb_V2/index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en-us">
+  <?php
+  header("Access-Control-Allow-Origin: *");
+?>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
